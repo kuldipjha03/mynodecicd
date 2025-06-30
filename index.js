4b866e093de1fcd4c3b53f6404f52c4e6e8cfe7b
jha.kuldip <jhakuldip40@gmail.com>
2025-06-30 23:01:34 +0530

    index.js new update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hi kuldip how are you');
+    res.send('Hi kuldip how are you this is update runing on pm2 auto');
 });
 
 app.listen(port, () => {
